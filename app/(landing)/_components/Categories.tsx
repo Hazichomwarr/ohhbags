db9d357c7398e234ab99c08559da927505a45b9a
hazichomwarr <batchiy6@gmail.com>
2026-03-27 08:15:54 -0400

    feat: add picture to Catrgories.tsx

--- app/(landing)/_components/Categories.tsx
+++ app/(landing)/_components/Categories.tsx
@@ -4,7 +4,7 @@ const categories = [
   { title: "African Clothing", img: "/clothes.jpg" },
   { title: "Jewelry", img: "/jewelry.jpg" },
   { title: "Fragrances", img: "/images/ohh_fragances.png" },
-  { title: "Accessories", img: "/accessories.jpg" },
+  { title: "Accessories", img: "/images/ohh_new.jpg" },
 ];
 
 export default function Categories() {
@@ -12,7 +12,7 @@ export default function Categories() {
     <section className="py-16 px-6">
       <h2 className="text-3xl mb-10 text-center font-serif">Our Collections</h2>
 
-      <div className="grid md:grid-cols-3 gap-6">
+      <div className="grid md:grid-cols-3 gap-6 ">
         {categories.map((cat) => (
           <div
             key={cat.title}
@@ -20,7 +20,7 @@ export default function Categories() {
           >
             <img
               src={cat.img}
-              className="w-full h-64 object-cover group-hover:scale-105 transition"
+              className="w-full h-64 object-cover group-hover:scale-105 transition hover:cursor-pointer"
             />
 
             <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
--- app/(landing)/_components/Footer.tsx
+++ app/(landing)/_components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="py-10 text-center border-t border-gray-800">
-      <p className="text-gray-400">© 2026 OHH Boutique — All rights reserved</p>
+      <p className="text-gray-400">© 2026 OHH Bags — All rights reserved</p>
     </footer>
   );
 }
